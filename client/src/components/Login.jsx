8faaa9fd3421f556a4d9d181fc221f92319851b1
sonal kumar mandal <152254912+SONALkumarmandal@users.noreply.github.com>
2025-02-10 21:47:55 +0530

    some

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -16,6 +16,7 @@ const Login = () => {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
+          "Access-Control-Allow-Credentials":true
         },
         body: JSON.stringify({
           email: email,
