3921da365ca41773ce54e65c2c97e71228032338
sonal kumar mandal <152254912+SONALkumarmandal@users.noreply.github.com>
2025-02-14 18:52:35 +0530

    img

--- client/src/Layouts/LoginLayout.jsx
+++ client/src/Layouts/LoginLayout.jsx
@@ -4,8 +4,8 @@ function LoginLayout() {
   return (
     <div className='flex items-center md:flex-row w-full min-h-screen  bg-zinc-900 sm:flex-col'>
         <div className='w-1/2 h-full flex-col'>
-                <div className='h-1/6 flex-col items-baseline justify-center -translate-y-16 translate-x-16'><img  className='w-32' src="/src/assets/img/logovalt.png" alt="logo" /></div>
-            <div className='flex justify-center'><img className='w-2/3 rounded-xl' src="/src/assets/img/logo.webp" alt="logo" /></div>
+                <div className='h-1/6 flex-col items-baseline justify-center -translate-y-16 translate-x-16'><img  className='w-32' src="/img/logovalt.png" alt="logo" /></div>
+            <div className='flex justify-center'><img className='w-2/3 rounded-xl' src="/img/logo.webp" alt="logo" /></div>
         </div>
         <div className='w-1/2 h-full flex items-center justify-center bg-zinc-900'>
                 <Outlet/>
