4a94e7bce15bfd231efee084dd0ec11fb5b275df
sonal kumar mandal <152254912+SONALkumarmandal@users.noreply.github.com>
2025-02-14 22:32:28 +0530

    basic img loading

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Loader() {
   return (
     <div className='w-full h-screen flex mt-60 justify-center'>
-        <img className='w-8 h-8' src="/src/assets/img/loader.gif" alt="loader" />
+        <img className='w-8 h-8' src="/public/img/loader.gif" alt="loader" />
     </div>
   )
 }
